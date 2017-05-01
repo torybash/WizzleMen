a274f6ecd78f05c0c59ad6479c0b5270f6dca3c9
torybash <torybash@gmail.com>
2017-05-01 13:48:07 +0200

    First!

---
+++ Assets/Scripts/WizzPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizzPlayer : MonoBehaviour {
+
+	private Rigidbody2D body;
+	private float moveX;
+
+	[SerializeField] private float speed;
+
+	void Awake(){
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		moveX = Input.GetAxisRaw("Horizontal");
+	}
+
+	void FixedUpdate(){
+		body.velocity = new Vector2(moveX * speed, body.velocity.y);
+	}
+}
